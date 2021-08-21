30dba902062aebc87d355305b30d59ddd690ce9d
vobinhan <binhan091020@gmail.com>
2021-08-22 03:38:49 +0700

    Updates

---
+++ precache-manifest.bce448a563133c55ddcb932edf93d12d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05a80d26714159d11bd6a2ab64953e1",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "8c05a94e66ff9ea65bce",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/home/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c05a94e66ff9ea65bce",
+    "url": "/home/static/js/main.7073bcb2.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "819ee89229ec757022f4770f2599b2ff",
+    "url": "/home/static/media/an.819ee892.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
